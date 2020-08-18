9fd021b812e89b0d53259e9d479c2aa728dfb35d
JCChen <james35022000@gmail.com>
2020-08-18 16:04:44 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -3,6 +3,10 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_PACKAGE_NAME := ConfigCenter
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v4 \
